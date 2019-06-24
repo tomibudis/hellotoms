897a1fee7dfe6d304519db8f6f8ab08c56f50dfd
tomibudis <tomibudis@gmail.com>
2019-06-24 16:05:23 +0700

    babel resolver config

--- .babelrc.js
+++ .babelrc.js
@@ -2,5 +2,18 @@ const env = require('./env-config')
 
 module.exports = {
   presets: ['next/babel'],
-  plugins: [['transform-define', env]]
+  plugins: [
+    ['transform-define', env],
+    [
+      'babel-plugin-module-resolver',
+      {
+        root: '.',
+        alias: {
+          '#src': './src',
+          '#components': './src/components',
+          '#view': './src/view',
+        },
+      },
+    ],
+  ],
 }
