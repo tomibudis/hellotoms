456d405a1a67194255fa5d67d080ddb2d69f3893
tomibudis <tomibudis@gmail.com>
2019-06-27 23:54:49 +0700

    Deploy Next.js to gh-pages

--- _next/static/chunks/styles.js
+++ _next/static/chunks/styles.js
@@ -1,14 +1,14 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
 
-/***/ "./src/view/styles.scss":
-/*!******************************!*\
-  !*** ./src/view/styles.scss ***!
-  \******************************/
+/***/ "./src/components/about/styles.scss":
+/*!******************************************!*\
+  !*** ./src/components/about/styles.scss ***!
+  \******************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 // extracted by mini-css-extract-plugin
-module.exports = {"title":"_1HQEnQKt9tqaCRHAXDf6WQ"};;
+module.exports = {"summaryInformation":"_3rsaTkU0rxv2_361Xu3rL5","labelInformation":"_3udjvA1nQHPncRzHuVf1rM","valueInformation":"weHbRPnMhEyiamWDKfXJf"};;
     if (true) {
       var injectCss = function injectCss(prev, href) {
         var link = prev.cloneNode();
@@ -30,7 +30,83 @@ module.exports = {"title":"_1HQEnQKt9tqaCRHAXDf6WQ"};;
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1561438603504");
+            injectCss(link, link.href.split("?")[0] + "?unix=1561654017456");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./src/components/profile/styles.scss":
+/*!********************************************!*\
+  !*** ./src/components/profile/styles.scss ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"descriptionProfile":"TT6bb2kmTm0q4TpLDieSR","tabs":"_3LDAnpbZrb6X_UAQmF1MAg"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1561654038431");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./src/components/workExperience/styles.scss":
+/*!***************************************************!*\
+  !*** ./src/components/workExperience/styles.scss ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"logoCompany":"_3m93hzUETXLL8i9ceg8rYB","titleCompany":"_23hextAD6QUCZeAllVwo8c","descriptionCompany":"_1UGkWk_b20d52yEO_a6zuh"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1561649015037");
           });
       }
     }
---
+++ _next/static/webpack/static/development/pages/index.js.69fd6d996a88b90b113a.hot-update.js
@@ -0,0 +1,103 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/components/workExperience/index.js":
+/*!************************************************!*\
+  !*** ./src/components/workExperience/index.js ***!
+  \************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles.scss */ "./src/components/workExperience/styles.scss");
+/* harmony import */ var _styles_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_scss__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! classnames */ "./node_modules/classnames/index.js");
+/* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(classnames__WEBPACK_IMPORTED_MODULE_3__);
+var _jsxFileName = "/Users/tomibudi/Documents/hellotoms/gh-pages/src/components/workExperience/index.js";
+
+
+
+
+
+var WorkExperience = function WorkExperience(props) {
+  var _props$data = props.data,
+      data = _props$data === void 0 ? [] : _props$data;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h5", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Work"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+    className: "list-group list-group-flush pt-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, data.length > 0 && data.map(function (row, index) {
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+      key: index,
+      className: "list-group-item pl-0 pb-0 pt-3 d-flex align-items-start",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 12
+      },
+      __self: this
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+      src: row.logo,
+      alt: "logo-company",
+      className: classnames__WEBPACK_IMPORTED_MODULE_3___default()('img-fluid rounded', _styles_scss__WEBPACK_IMPORTED_MODULE_1___default.a.logoCompany),
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 13
+      },
+      __self: this
+    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      className: "pl-3",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 18
+      },
+      __self: this
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", {
+      className: classnames__WEBPACK_IMPORTED_MODULE_3___default()('mb-0', _styles_scss__WEBPACK_IMPORTED_MODULE_1___default.a.titleCompany),
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 19
+      },
+      __self: this
+    }, row.company && row.company), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+      className: _styles_scss__WEBPACK_IMPORTED_MODULE_1___default.a.descriptionCompany,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 20
+      },
+      __self: this
+    }, row.as && row.as)));
+  })));
+};
+
+WorkExperience.propTypes = {
+  data: Object(prop_types__WEBPACK_IMPORTED_MODULE_2__["arrayOf"])(Object(prop_types__WEBPACK_IMPORTED_MODULE_2__["shape"])({
+    logo: prop_types__WEBPACK_IMPORTED_MODULE_2__["string"],
+    company: prop_types__WEBPACK_IMPORTED_MODULE_2__["string"],
+    as: prop_types__WEBPACK_IMPORTED_MODULE_2__["string"]
+  }))
+};
+/* harmony default export */ __webpack_exports__["default"] = (WorkExperience);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.69fd6d996a88b90b113a.hot-update.js.map
